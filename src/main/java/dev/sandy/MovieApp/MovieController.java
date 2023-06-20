644b150c0d020283706b1c0a3f04984327c9b38a
5andeepNambiar <sandeepnambiar02@gmail.com>
2023-06-20 12:37:20 +0530

    configured cors and review codes

--- src/main/java/dev/sandy/MovieApp/MovieController.java
+++ src/main/java/dev/sandy/MovieApp/MovieController.java
@@ -2,11 +2,10 @@ package dev.sandy.MovieApp;
 
 import java.util.List;
 import java.util.Optional;
-
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
